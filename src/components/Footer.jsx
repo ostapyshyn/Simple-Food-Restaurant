9c92b689f8f0a1f27a654e28f793a0f93d62026e
ostapyshyn <ostapyshyn@gmail.com>
2023-12-29 16:27:47 +0200

    Add new images and components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Made by
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer">
+          Cheeky Coding Company
+        </a>
+        ™.
+      </p>
+    </footer>
+  )
+}
